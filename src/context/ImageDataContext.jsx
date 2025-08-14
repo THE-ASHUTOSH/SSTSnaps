c38f3c0be2ee9d4c7e8e48dae2c3a67d46fa4ffd
Ashutosh Kumar <82095076+THE-ASHUTOSH@users.noreply.github.com>
2025-08-14 10:07:03 +0530

    randomized the images

--- src/context/ImageDataContext.jsx
+++ src/context/ImageDataContext.jsx
@@ -13,7 +13,14 @@ const ImageDataContextProvider = ({ children }) => {
     const [EventdefID, setEventdefID] = useState()
     const [imageArr, setimages] = useState([]);
 
-    
+    function shuffleArray(array) {
+      let shuffled = [...array]; 
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    }
 
         useEffect(() => {
             const fetchImages = () => {
@@ -21,7 +28,8 @@ const ImageDataContextProvider = ({ children }) => {
                 const event = eventsArr.find((event) => event.defid === EventdefID);
                 // console.log(event, "event")
                 if (event && event.imageArr) {
-                    setimages(event.imageArr);
+                    // setimages(event.imageArr);
+                    setimages(shuffleArray(event.imageArr));
                 } else {
                     setimages([]); // Set empty array if no matching event found
                 }
